0e3cc203d2fc37970dbbdbd9bdfd0c8d1202d8f0
Achal Choudhary <choudharyachal52@gmail.com>
2025-11-12 00:35:46 +0530

    fix: changed button color to green

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 console.log("Weather App loaded");
+document.getElementById('search-btn').addEventListener('click',function() {
+    const city = document.getElementById('city-input').value;
+    console.log('City entered:',city);
+});
